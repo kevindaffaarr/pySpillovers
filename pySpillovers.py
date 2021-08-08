0f46f358cf7873fcd2c086b95dc5eba1998570bc
kevindaffaarr <kevindaffaar@gmail.com>
2021-08-08 15:03:55 +0700

    initial commit

---
+++ pySpillovers.py
@@ -0,0 +1,52 @@
+# ==============================
+# IMPORT PACKAGE
+# ==============================
+
+# ==============================
+# USER INPUT
+# ==============================
+
+# ==============================
+# IMPORT DATA
+# ==============================
+# Import sectors
+
+# Import sectorData
+
+# reIndex sectorData with Date
+
+# Filter sectorData between DateTo and DateFrom
+
+# ==============================
+# ==DIEBOLD 2012 SPILLOVERS INDEX==
+# ==============================
+# ==============================
+# DATA PREPARATION BASED ON OUTPUTMODE
+# ==============================
+
+# ==============================
+# STATISTIC OF DATA
+# ==============================
+
+# ==============================
+# FORECAST ERROR VARIANCE DECOMPOSITIONS (FEVD)
+# ==============================
+
+# ==============================
+# TOTAL, DIRECTIONAL, NET SPILLOVERS
+# ==============================
+
+# ==============================
+# SENSITIVITY ANALYSIS
+# ==============================
+
+# ==============================
+# OUTPUT
+# ==============================
+# Volatility or Return Table and Graph
+
+# Data Spillover Table
+
+# Total Data Spillover Table and Graph
+
+# Directional Volatility Spillovers FROM, TO, and NET Each, NET Pairwaise Table and Graph
